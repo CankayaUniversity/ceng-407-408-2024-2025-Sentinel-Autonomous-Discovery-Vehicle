893b0fd01102733c5522e43ab321403385b2d0a7
UtkuAltnkaya <utkualt06@outlook.com>
2025-04-12 18:35:41 +0300

    Update sentinel launch file

--- sentinel.computer/src/sentinel/launch/sim.launch.py
+++ sentinel.computer/src/sentinel/launch/sim.launch.py
@@ -1,95 +1,172 @@
-import os
+from typing import List
 
-from ament_index_python.packages import get_package_share_directory
-
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    TimerAction,
+)
 from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.substitutions import FindPackageShare
 
 
-def generate_launch_description():
+def declare_args() -> List[DeclareLaunchArgument]:
+    return [
+        DeclareLaunchArgument(
+            "use_ros2_control",
+            default_value="true",
+            description="Use ros2_control instead of gazebo_control plugin",
+        ),
+        DeclareLaunchArgument(
+            "use_autonomous",
+            default_value="true",
+            description="Enable autonomous movement with exploration",
+        ),
+        DeclareLaunchArgument(
+            "use_builtin",
+            default_value="true",
+            description="Use builtin manual controller (teleop nodes)",
+        ),
+        DeclareLaunchArgument(
+            "use_3d_map",
+            default_value="true",
+            description="Enable 3D map",
+        ),
+        DeclareLaunchArgument(
+            "timer_period",
+            default_value="30.0",
+            description="Timer Period",
+        ),
+        DeclareLaunchArgument(
+            "world",
+            default_value="edifice.sdf",
+            description="World name to load",
+        ),
+    ]
 
+
+def generate_launch_description() -> LaunchDescription:
     use_sim_time = "true"
+    use_ros2_control = LaunchConfiguration("use_ros2_control")
+    use_autonomous = LaunchConfiguration("use_autonomous")
+    use_builtin = LaunchConfiguration("use_builtin")
+    use_3d_map = LaunchConfiguration("use_3d_map")
+    timer_period = LaunchConfiguration("timer_period")
+    world = LaunchConfiguration("world")
 
-    model = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory("model"), "launch", "rsp.launch.py"
-                )
-            ]
-        ),
+    model = get_launch_file(
+        package_name="model",
+        launch_file_name="rsp.launch.py",
         launch_arguments={
             "use_sim_time": use_sim_time,
-            "use_ros2_control": "true",
-        }.items(),
+            "use_ros2_control": use_ros2_control,
+            "use_3d_map": use_3d_map,
+        },
     )
 
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory("simulation"),
-                    "launch",
-                    "gazebo.launch.py",
-                )
-            ]
-        ),
+    gazebo = get_launch_file(
+        package_name="simulation",
+        launch_file_name="gazebo.launch.py",
         launch_arguments={
             "use_sim_time": use_sim_time,
-            "use_ros2_control": "true",
-        }.items(),
+            "world": world,
+        },
     )
 
-    movement = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory("movement"),
-                    "launch",
-                    "movement.launch.py",
-                )
-            ]
-        ),
+    movement = get_launch_file(
+        package_name="movement",
+        launch_file_name="movement.launch.py",
         launch_arguments={
             "use_sim_time": use_sim_time,
-            "use_builtin": "true",
-            "use_explore": "true",
-        }.items(),
+            "use_builtin": use_builtin,
+            "use_autonomous": use_autonomous,
+        },
     )
 
-    mapping = IncludeLaunchDescription(
+    mapping = get_launch_file(
+        package_name="mapping",
+        launch_file_name="mapping.launch.py",
+        launch_arguments={
+            "use_sim_time": use_sim_time,
+            "use_3d_map": use_3d_map,
+        },
+    )
+
+    rviz2 = get_rviz2(use_3d_map)
+
+    ros2_control = load_ros2_control(use_ros2_control)
+
+    timer_action = TimerAction(
+        period=timer_period, actions=ros2_control + [movement, mapping] + rviz2
+    )
+
+    ld = LaunchDescription(declare_args())
+    ld.add_action(model)
+    ld.add_action(gazebo)
+    ld.add_action(timer_action)
+
+    return ld
+
+
+def get_launch_file(
+    package_name: str, launch_file_name: str, launch_arguments: dict
+) -> IncludeLaunchDescription:
+    return IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory("mapping"),
-                    "launch",
-                    "slam_toolbox.mapping.launch.py",
-                )
-            ]
+            PathJoinSubstitution(
+                [FindPackageShare(package_name), "launch", launch_file_name]
+            )
         ),
-        launch_arguments={"use_sim_time": use_sim_time}.items(),
+        launch_arguments=launch_arguments.items(),
     )
 
-    rviz_config_path = os.path.join(
-        get_package_share_directory("sentinel"), "config", "simulation.rviz"
-    )
-    rviz2 = Node(
+
+def get_rviz2(use_3d_map: LaunchConfiguration) -> List[Node]:
+    rviz2_2d = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
         output="screen",
-        arguments=["-d", rviz_config_path],
+        arguments=[
+            "-d",
+            PathJoinSubstitution(
+                [FindPackageShare("sentinel"), "config", "simulation.rviz"]
+            ),
+        ],
+        condition=UnlessCondition(use_3d_map),
     )
 
-    return LaunchDescription(
-        [
-            model,
-            gazebo,
-            movement,
-            mapping,
-            rviz2,
-        ]
+    rviz2_3d = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=[
+            "-d",
+            PathJoinSubstitution(
+                [FindPackageShare("sentinel"), "config", "3d_map.rviz"]
+            ),
+        ],
+        condition=IfCondition(use_3d_map),
     )
+
+    return [rviz2_2d, rviz2_3d]
+
+
+def load_ros2_control(use_ros2_control: LaunchConfiguration):
+    joint_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+        condition=IfCondition(use_ros2_control),
+    )
+    skid_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["skid_steer_cont"],
+        condition=IfCondition(use_ros2_control),
+    )
+    return [joint_spawner, skid_spawner]
